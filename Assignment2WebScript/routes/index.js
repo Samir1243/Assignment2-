f77b1754550e70e0363e52350b129aa9058f7ecf
Samir1243 <samir.khaksar@ontariotechu.net>
2025-11-03 20:13:42 -0500

    Fix changes with nodemon stuff

--- Assignment2WebScript/routes/index.js
+++ Assignment2WebScript/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'home' });
+});
+
+/* GET home page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+/* GET home page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'project' });
+});
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// server.mjs
-import { createServer } from 'node:http';
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('SM BAHA!\n');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
